fbaac7bda73160e344e4b4981cb92f1c7df19ee7
feryrmdhnc <feryramadhan555@gmail.com>
2020-10-20 15:28:43 +0800

    react-rt-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Headers from '../src/components/Headers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Headers />
+        <Switch>
+          <h2>Home Page</h2>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
