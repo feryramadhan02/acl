6ebc78c48cb4da4e22d923658f2d0bc61388120d
feryrmdhnc <feryramadhan555@gmail.com>
2020-10-22 15:25:48 +0800

    branch develop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Headers from '../src/components/Headers';
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Headers />
         <Switch>
           <h2>Home Page</h2>
         </Switch>
