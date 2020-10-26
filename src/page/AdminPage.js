e54b76dc34958ed159c3327cb8c07ce41e514349
feryrmdhnc <feryramadhan555@gmail.com>
2020-10-26 11:43:26 +0800

    success get add del dummy data

--- src/page/AdminPage.js
+++ src/page/AdminPage.js
@@ -5,15 +5,6 @@ import Form from 'react-bootstrap/Form';
 import '../assets/style/adminpage.scss';
 
 const AdminPage = () => {
-    const [userData, setUserData] = useState({
-        id: null,
-        nama: "",
-        email: "",
-        role: "User"
-    })
-    const [dt, setDt] = useState([])
-
-
     const userDummy = [
         {
             id: 1,
@@ -35,9 +26,18 @@ const AdminPage = () => {
         },
     ]
 
-    useEffect(() => {
-        setUserData([...dt])
-    }, [dt])
+    const [userData, setUserData] = useState({
+        id: 1,
+        nama: "",
+        email: "",
+        role: "User"
+    })
+    const [dt, setDt] = useState([])
+
+
+    // useEffect(() => {
+    //     setUserData([...dt])
+    // }, [dt])
 
     const handleInput = (e) => {
         setUserData({
@@ -50,18 +50,17 @@ const AdminPage = () => {
         e.preventDefault()
         setDt(dt.concat(userData))
         setUserData({
-            id: null,
+            id: userData.id + 1,
             nama: "",
             email: "",
             role: "User"
         })
-        console.log(setUserData.id)
     }
 
-    const deleteUser = (id) => {
-        console.log('del', id)
+    const deleteUser = id => {
         alert('Are you sure ?')
-        setDt(dt.filter(userData => userData.id !== id))
+        setDt(dt.filter((user) => user.id !== id))
+        console.log('del', id)
     }
 
     return (
@@ -109,9 +108,9 @@ const AdminPage = () => {
                     ))}
                 </div>
                 <div className="data">
-                    {dt.map((data) => (
-                        <div key={data.id} className="data-user">
-                            <p>Nama : <span>{data.nama}</span><svg onClick={() => deleteUser(dt.id)} width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    {dt.map((item) => (
+                        <div key={item.id} className="data-user">
+                            <p>Nama : <span>{item.nama}</span><svg onClick={() => deleteUser(item.id)} width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                             </svg>
@@ -119,8 +118,8 @@ const AdminPage = () => {
                                     <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </p>
-                            <p>Email : <span>{data.email}</span></p>
-                            <p>Role : <span>{data.role}</span></p>
+                            <p>Email : <span>{item.email}</span></p>
+                            <p>Role : <span>{item.role}</span></p>
                         </div>
                     ))}
                 </div>
