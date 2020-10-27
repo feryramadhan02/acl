ad9a6064ad4e5890d000478dc2100ece8ec5a9d6
Petani Kode <ajpratama.jn@gmail.com>
2020-10-27 13:31:23 +0800

    'tes'

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 // import LoginAdmin from './Components/LoginAdmin';
 // import Daftar from './Components/Daftar';
 // import { Route, Switch } from 'react-router-dom';
-import './index.css';
+// import './index.css';
 // import Dash from './Components/Dash';
-// import Footer from './Components/Footer'
 
-// import Nav from './Components/Nav'
 import Routes from '../src/router/Routes.js';
 import { BrowserRouter } from 'react-router-dom'; 
 
